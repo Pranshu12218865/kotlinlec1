225fc494ded7f8c38525ac48537cf3038d8962cf
Pranshu12218865 <118091957+Pranshu12218865@users.noreply.github.com>
2024-08-15 23:50:08 +0530

    Add files via upload

---
+++ v1.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>){
+    // val asc=Array(4,{i -> i*2})
+    // print(asc.contentToString())
+    // var fnum=122
+    // val fnum1:Long=fnum.toLong()
+    // print(fnum1)
+    print("Enter the name: ")
+    val name=readLine()
+    print("Enter the age: ")
+    val age =readLine()
+
+    print("----------My name is $name and my age is $age---------------")
+}
